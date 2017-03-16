53e7fe0dadbb3364a235554771fa0196c3b4d52f
h179738 <h179738@ADVORNICHENKO.rdx.lgc.com>
2017-03-16 12:02:50 +0200

    person info added

--- src/com/eclipsercp/swtjface/HelloJFace.java
+++ src/com/eclipsercp/swtjface/HelloJFace.java
@@ -10,14 +10,19 @@ import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.window.ApplicationWindow;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
 
 public class HelloJFace extends ApplicationWindow {
 
@@ -33,31 +38,24 @@ public class HelloJFace extends ApplicationWindow {
 	@Override
 	protected Control createContents(Composite parent) {
 
-		//Text helloText = new Text(parent, SWT.CENTER);
-		//helloText.setText("Hello SWT and JFace!");
-		
 		getShell().setText("JFace homework log");
 		parent.setSize(500, 500);
-		parent.setLayout(new GridLayout(3, true));
-
-		createViewer(parent);
+		parent.setLayout(new GridLayout(1, true));
 		
-		Button swtDoneBtn = new Button(parent, SWT.CHECK);
-		swtDoneBtn.setText("SWT task done");
-		swtDoneBtn.pack();
-		
-		RadioGroupFieldEditor rgfe = new RadioGroupFieldEditor(
-				"UserChoice", "Choose an option:", 3,
-				new String[][] {{"Choice1", "ch1"},
-				{"Choice2", "ch2"},
-				{"Choice3", "ch3"}},
-				parent, true);
+		SashForm sf = new SashForm(parent, SWT.HORIZONTAL);
 
+		// list of persons
+		createListOfPersons(sf);
+		
+		//person info
+		createPersonInfo(sf);				
+		
+		
 		parent.pack();
 		return parent;
 	}
 
-	private void createViewer(Composite parent) {
+	private void createListOfPersons(Composite parent) {
 		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
 		createColumns(parent, viewer);
 
@@ -77,6 +75,61 @@ public class HelloJFace extends ApplicationWindow {
 		gridData.grabExcessVerticalSpace = true;
 		gridData.horizontalAlignment = GridData.FILL;
 		viewer.getControl().setLayoutData(gridData);
+	}
+	
+	private void createPersonInfo(Composite parent) {
+		GridData gridDataResult = new GridData();
+		gridDataResult.horizontalAlignment = GridData.FILL;
+		
+		Group personInfoGroup = new Group(parent, SWT.SHADOW_ETCHED_OUT);
+		personInfoGroup.setText("Person info");
+		personInfoGroup.setLayout(new GridLayout(2, true));
+		personInfoGroup.setLayoutData(gridDataResult);
+
+		Label labelPersonName = new Label(personInfoGroup, SWT.NONE);
+		labelPersonName.setText("Name:");
+
+		Text personName = new Text(personInfoGroup, SWT.BORDER | SWT.RIGHT);
+
+		Label labelGroupName = new Label(personInfoGroup, SWT.NONE);
+		labelGroupName.setText("Group #:");
+
+		Text personGroup = new Text(personInfoGroup, SWT.BORDER | SWT.RIGHT);
+		personGroup.addListener(SWT.Verify, event -> checkDigitalInput(event));
+
+		Label labelSwtDone = new Label(personInfoGroup, SWT.NONE);
+		labelSwtDone.setText("SWT task done");
+
+		Button swtDoneBtn = new Button(personInfoGroup, SWT.CHECK | SWT.RIGHT_TO_LEFT);
+		swtDoneBtn.setLayoutData(gridDataResult);
+
+		personInfoGroup.pack();
+
+		// button 'New'
+		Button newBtn = new Button(personInfoGroup, SWT.PUSH);
+		newBtn.setText("New");
+		newBtn.addListener(SWT.Selection,
+				event -> addPerson(personName.getText(), personGroup.getText(), swtDoneBtn.getSelection()));
+	}
+	
+	private void addPerson(String nameString, String groupString, Boolean swtDone) {
+		if (nameString.length() == 0) {
+			return;
+		}
+
+		Integer group = 0;
+		if (groupString.length() > 0) {
+			try {
+				group = Integer.parseInt(groupString);
+			} catch (NumberFormatException e) {
+			}
+		}
+
+		Person newPerson = new Person(nameString, group, swtDone);
+		personList.add(newPerson);
+
+		viewer.refresh();
+
 	}
 
 	// create the columns for the table
@@ -124,6 +177,19 @@ public class HelloJFace extends ApplicationWindow {
 		column.setResizable(true);
 		column.setMoveable(true);
 		return viewerColumn;
+	}
+	
+	private void checkDigitalInput(Event e) {
+
+		String string = e.text;
+		char[] chars = new char[string.length()];
+		string.getChars(0, chars.length, chars, 0);
+		for (int i = 0; i < chars.length; i++) {
+			if (!('0' <= chars[i] && chars[i] <= '9')) {
+				e.doit = false;
+			}
+		}
+
 	}
 
 	public void setFocus() {
